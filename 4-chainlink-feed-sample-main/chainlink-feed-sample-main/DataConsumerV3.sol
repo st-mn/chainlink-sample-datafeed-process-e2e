c80192913f856e726c986070ebe0a5b46b1982be
st-mn <56998558+st-mn@users.noreply.github.com>
2023-11-25 16:06:54 +0100

    Update DataConsumerV3.sol

--- 4-chainlink-feed-sample-main/chainlink-feed-sample-main/DataConsumerV3.sol
+++ 4-chainlink-feed-sample-main/chainlink-feed-sample-main/DataConsumerV3.sol
@@ -1,8 +1,3 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED
  * VALUES FOR CLARITY.
@@ -19,10 +14,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
  * page for details.
  */
 
-contract DataConsumerV3 {
-    AggregatorV3Interface internal dataFeed;
-
-    /**
+ /**
      * Network: Sepolia
      * Aggregator: BTC/USD
      * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
@@ -30,17 +22,23 @@ contract DataConsumerV3 {
      * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
      */
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract DataConsumerV3 {
+    AggregatorV3Interface internal dataFeed;
+
+
     constructor() {
         dataFeed = AggregatorV3Interface(
             0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
         );
     }
 
-    /**
-     * Returns the latest answer.
-     */
+
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
-        // prettier-ignore
         (
             /* uint80 roundID */,
             int answer,
