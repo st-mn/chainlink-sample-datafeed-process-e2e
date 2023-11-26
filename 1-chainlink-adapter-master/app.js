a3b65206c4ee0dbc0a4168fe211752f46041f51b
st-mn <56998558+st-mn@users.noreply.github.com>
2023-11-26 12:34:38 +0100

    Update app.js

--- 1-chainlink-adapter-master/app.js
+++ 1-chainlink-adapter-master/app.js
@@ -1,13 +1,13 @@
 /**
- * Express application to handle incoming requests and communicate with the OpenWeatherMap external adapter.
+ * Express application to handle incoming requests and communicate with the external adapter.
  *
  * This application utilizes the 'dotenv' package to load environment variables from a '.env' file.
  * It also imports the 'createRequest' function from the 'index' module, which is responsible for generating
- * and sending HTTP requests to the OpenWeatherMap API.
+ * and sending HTTP requests to the  API.
  *
  * The Express application defines two endpoints:
  *
- * 1. POST /: This endpoint receives a JSON body containing the request parameters, including the city name.
+ * 1. POST /: This endpoint receives a JSON body containing the request parameters.
  *    It then calls the 'createRequest' function to fetch the weather data and returns the response to the client.
  *
  * 2. GET /health: This endpoint returns a simple 'OK' response to indicate that the application is healthy.
